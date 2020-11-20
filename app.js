9346207c1c0ee672c05c1f3d12b3d5e0fac4703d
cookieninja <github@nomoreheroes.org.uk>
2020-11-20 19:40:10 +0000

    change message 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello cruel World!'));
+app.get('/', (req, res) => res.send('Hello cruel World! Am I being monitored and edited???'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
